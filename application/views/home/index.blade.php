7ed25d767fa4459a1a923edf1abf8cf58eee9200
jonbr858 <jonbr858@student.liu.se>
2013-04-18 18:02:18 +0200

    Added ember

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -1,57 +1,76 @@
 <!doctype html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Laravel: A Framework For Web Artisans</title>
-	<meta name="viewport" content="width=device-width">
-	{{ HTML::style('laravel/css/style.css') }}
-</head>
-<body>
-	<div class="wrapper">
-		<header>
-			<h1>Laravel</h1>
-			<h2>A Framework For Web Artisans</h2>
-
-			<p class="intro-text" style="margin-top: 45px;">
-			</p>
-		</header>
-		<div role="main" class="main">
-			<div class="home">
-				<h2>Learn the terrain.</h2>
-
-				<p>
-					You've landed yourself on our default home page. The route that
-					is generating this page lives at:
-				</p>
-
-				<pre>{{ path('app') }}routes.php</pre>
-
-				<p>And the view sitting before you can be found at:</p>
-
-				<pre>{{ path('app') }}views/home/index.blade.php</pre>
-
-				<h2>Grow in knowledge.</h2>
-
-				<p>
-					Learning to use Laravel is amazingly simple thanks to
-					its {{ HTML::link('docs', 'wonderful documentation') }}.
-				</p>
-
-				<h2>Create something beautiful.</h2>
-
-				<p>
-					Now that you're up and running, it's time to start creating!
-					Here are some links to help you get started:
-				</p>
-
-				<ul class="out-links">
-					<li><a href="http://laravel.com">Official Website</a></li>
-					<li><a href="http://forums.laravel.com">Laravel Forums</a></li>
-					<li><a href="http://github.com/laravel/laravel">GitHub Repository</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>ODOT:TODO</title>
+        <meta name="viewport" content="width=device-width">
+        <style>
+            *{ 
+                margin:0;
+                padding:0;
+            }
+            html,body {
+                height: 100%;
+                background-color: #333;
+            }
+            .left-list {
+                float: left;
+                width: 200px;
+                background-color: #195f91;
+                height: 100%;
+                overflow: hidden;
+            }
+            .container {
+                margin: 50px auto 0 auto;
+                width: 800px;
+            }
+            .main-container {
+                float: left;
+                background-color: #72ADD4;
+                overflow: hidden;
+                width: 500px;
+            }
+            .task-list {
+                margin: 0 auto 0 auto;
+                border: 1px solid black;
+                min-height: 300px;
+                background-color: #2972A3;
+            }
+            li {
+                border-left: 1px solid black;
+                padding: 10px;
+                font-size: 20px;
+                color: #eee;
+            }
+            li:hover {
+                background-color: #72ADD4;
+                color: green;
+                text-align: center;
+            }
+        </style>
+        
+    </head>
+    <body>
+        <div class="container">
+            <div class="left-list">
+                <ul>
+                    <li>FIRST</li>
+                    <li>SECOND</li>
+                    <li>THIRD</li>
+                    <li>FOURTH</li>
+                    <li>FIFTH</li>
+                </ul>
+            </div>
+            <div class="main-container">
+                <div class="task-list">
+                    <p>FIRST</p>
+                    <p>SECOND</p>
+                    <p>THIRD</p>
+                    <p>FOURTH</p>
+                    <p>FIFTH</p>
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
