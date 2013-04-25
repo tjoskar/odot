6d1fd106fb47d8e746354f5e78748f53c539c08a
Oskar Karlsson <mr.karlsson.oskar@gmail.com>
2013-04-25 09:30:45 +0200

    Added "Awessome font" and made it possible to hide/show subitems

--- public/js/app.js
+++ public/js/app.js
@@ -138,14 +138,24 @@ App.Views.Lists = Backbone.View.extend({
 App.Views.Item = Backbone.View.extend({
   tagName: 'li',
   template: _.template(
-    '<h3><%= title %></h3>' +
+    '<h3><i class="icon-circle-blank"></i><%= title %></h3>' +
+    '<div class="item-button-holder"><i class="icon-cog"></i></div>' +
     '<% if( !_.isEmpty(sub_items) ) { %>' +
-      '<div class="sub-items"><ul>' +
-        '<% _.each(sub_items, function(sub_item) { %>' +
-          '<li><%= sub_item.title %></li>' +
-        '<% }); %>' +
-      '</ul></div>' +
-    '<% } %>'
+      '<div class="sub-items">' +
+        '<p class="item-description"><%= description %> </p>' +
+        '<ul>' +
+          '<% _.each(sub_items, function(sub_item) { %>' +
+            '<li>' +
+              '<p><%= sub_item.title %></p>' +
+              '<div class="item-button-holder"><i class="icon-cog"></i>' +
+            '</li>' +
+          '<% }); %>' +
+        '</ul>' +
+      '</div>' +
+    '<% } %>' +
+    '<div class="item-settings">' +
+
+    '</div>'
     ),
 
   render: function() {
@@ -154,11 +164,18 @@ App.Views.Item = Backbone.View.extend({
   },
 
   events : {
-    "click" : "itemClicked"
+    "click h3" : "showSubitem",
+    "click .icon-cog" : "showItemSettings"
   },
 
-  listClicked: function(event) {
-    console.log('OMG');
+  showSubitem: function() {
+    $(this.el).find('.item-settings').slideUp("slow");
+    $(this.el).find('.sub-items').slideToggle("slow");
+  },
+
+  showItemSettings: function() {
+    $(this.el).find('.sub-items').slideUp("slow");
+    $(this.el).find('.item-settings').slideToggle("slow");
   }
 });
 
