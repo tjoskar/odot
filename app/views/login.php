c25662560a97639c2a82fa190d11e2084d800a94
Jonas <jonbr858@student.liu.se>
2013-05-13 16:43:43 +0200

    Changed facebook js src

--- app/views/login.php
+++ app/views/login.php
@@ -234,8 +234,8 @@ window.fbAsyncInit = function() {
     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
     if (d.getElementById(id)) {return;}
     js = d.createElement('script'); js.id = id; js.async = true;
-    //js.src = "//connect.facebook.net/en_US/all.js";
-    js.src = "//connect.facebook.net/en_US/all/debug.js";
+    js.src = "//connect.facebook.net/en_US/all.js";
+    //js.src = "//connect.facebook.net/en_US/all/debug.js";
     ref.parentNode.insertBefore(js, ref);
 }(document));
 
